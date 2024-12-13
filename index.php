2362f82ce9a0549baa5ccd6d81952174323e6b47
Xcaliburr-AT <2020120085@psu.palawan.edu.ph>
2024-12-13 17:36:42 +0800

    html

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php
 session_start();
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Number Guessing Game</title>
+</head>
+<body>
+    <h1>Number Guessing Game</h1>
+    <h2>Try to guess the hidden number</h2>
+    <p>Hint: the number is only between 1 and 10!</p>
+
+</body>
+</html>
